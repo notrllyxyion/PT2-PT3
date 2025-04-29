7f25756bcb3ac4205d155d3102906fbf1c77fee3
CIITSHSDTP00\CIIT <it@ciit.edu.ph>
2025-04-29 13:35:42 +0800

    Initial commit

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <form action = "index.php" method = "get">
+        <input type = "checkbox" name="Chicken" value = "Chicken" > Chicken <br>
+        <input type = "checkbox" name = "Shawarma" value = "Shawarma"> Shawarma <br>
+        <input type = "checkbox" name = "FrenchFries" value = "French Fries">French Fries<br>
+        <input type = "checkbox" name = "MashedSoup" value = "Mashed Soup"> Mashed Soup<br>
+        <input type = "checkbox" name = "Sisig" value = "Sisig">Sisig<br>
+        <input type = "submit" name="submit">
+    </form>
+</body>
+</html>
+
+
+
+<?php 
+    $order = array();
+    if (isset($_GET["submit"])) {
+        if (isset($_GET["Chicken"])) {
+            array_push($order, $_GET["Chicken"] . " 500");
+        }
+        if (isset($_GET["Shawarma"])) {
+            array_push($order, $_GET["Shawarma"] ." 500"); 
+        }
+        if (isset($_GET["FrenchFries"])) {
+            array_push($order, $_GET["FrenchFries"] ." 2131122");
+        }
+        if (isset($_GET["MashedSoup"])) {
+            array_push($order, $_GET["MashedSoup"] ." 50000");
+        }
+        if (isset($_GET["Sisig"])) {
+            array_push($order, $_GET["Sisig"] ." 24332423432");
+        }
+        foreach ($order as $k => $v) 
+        {
+            echo " {$k} . {$v} <br>";
+        }
+        
+    }
+   
+
+
+?>
